9c1d47ab7aea889842b07780125fd3c3ac5ba5f2
code1990 <s1332177151@sina.com>
2020-04-01 22:40:54 +0800

    init

---
+++ Kotlin/HelloApp/app/src/main/java/com/example/helloapp/Chapter0852OptionsMenuActivity.kt
@@ -0,0 +1,15 @@
+package com.example.helloapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Chapter0852OptionsMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spinner_dialog)
+
+
+    }
+
+}
---
+++ Kotlin/HelloApp/app/src/main/java/com/example/helloapp/Chapter1042ContentResolverActivity.kt
@@ -0,0 +1,15 @@
+package com.example.helloapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Chapter1042ContentResolverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spinner_dialog)
+
+
+    }
+
+}
